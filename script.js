218f65618a9bf69102f7870300ab07c549b3cee1
Araame <arame171002@gmail.com>
2026-06-01 21:48:29 +0000

    HTML done

---
+++ script.js
@@ -0,0 +1,10 @@
+
+//Recupere form
+form = document.querySelector("form");
+let nameInput = document.getElementById("name");
+let surnameInput = document.getElementById("surname");
+let emailInput = document.getElementById("email");
+let profileInput = document.getElementById("profile");
+let typePersonInput = document.querySelector("typePerson");
+let hobbyInput = document.querySelector("hobby");
+let storytimeInput = document.getElementById("storytime");
